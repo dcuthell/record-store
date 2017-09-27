3c77efa089176a44c82e90aa1d8041ec6ae9b7d4
dcuthell <dcuthell@pdx.edu>
2017-09-27 10:06:45 -0700

    add edit and delete functionality

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,20 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
-import { ALBUMS } from './mock-albums';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class AlbumService {
 
-  constructor() { }
-  getAlbums() {
-      return ALBUMS;
+  albums: FirebaseListObservable<any[]>;
+
+  constructor(private database: AngularFireDatabase) {
+    this.albums = database.list('albums');
   }
 
-  getAlbumById(albumId: number){
-  for (var i = 0; i <= ALBUMS.length - 1; i++) {
-    if (ALBUMS[i].id === albumId) {
-      return ALBUMS[i];
-    }
+  getAlbums() {
+      return this.albums;
   }
-}
+
+  addAlbum(newAlbum: Album) {
+    this.albums.push(newAlbum);
+  }
+
+  getAlbumById(albumId: string){
+    return this.database.object('albums/' + albumId)
+  }
+
+  updateAlbum(localUpdatedAlbum){
+    let albumEntryInFirebase = this.getAlbumById(localUpdatedAlbum.$key);
+    albumEntryInFirebase.update({title: localUpdatedAlbum.title,
+                                artist: localUpdatedAlbum.artist,
+                                description: localUpdatedAlbum.description});
+  }
+
+  deleteAlbum(localAlbumToDelete){
+    let albumEntryInFirebase = this.getAlbumById(localAlbumToDelete.$key);
+    albumEntryInFirebase.remove();
+  }
 }
--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { Router } from '@angular/router';
 import { AlbumService } from '../album.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-marketplace',
@@ -10,13 +11,16 @@ import { AlbumService } from '../album.service';
   providers: [AlbumService]
 })
 export class MarketplaceComponent implements OnInit {
+
+  albums: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
+
   constructor(private router: Router, private albumService: AlbumService){}
 
-  albums: Album[];
   ngOnInit(){
     this.albums = this.albumService.getAlbums();
    }
-   goToDetailPage(clickedAlbum: Album) {
-    this.router.navigate(['albums', clickedAlbum.id]);
+   goToDetailPage(clickedAlbum) {
+    this.router.navigate(['albums', clickedAlbum.$key]);
   };
 }
